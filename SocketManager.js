d4e50b4a32f3725a86dd58d0989e5c212a5d72ed
roopam527 <roopamg777@gmail.com>
2019-07-11 15:03:08 +0530

    message

--- SocketManager.js
+++ SocketManager.js
@@ -1,11 +1,36 @@
+let onlineUsers = {};
+
+const { 
+  USER_CONNECTED,
+  
+} = require("./events");
 const SocketManager = (socket)=>{
     global.socket = socket
     console.log(socket.id)
    
-    socket.on("new message",(name)=>{
-      console.log(name);
-    })
+    socket.on(USER_CONNECTED, async (userId) => {
+      //const userInfo = await User.findById(userId);
+      const user = createUser({  socketId:socket.id , userId});
+      io.emit(`${userId}-connected`,true)
+      addUserToOnlineUsersList(user);
+     // const chatHistory = await getAllPeviousChats(userInfo.chatsIdArray);
+    
+    //  setUser(user, chatHistory);
+    });
     };
 
+const createUser = ({  socketId = null , userId = ""} = {}) => ({
       
+      socketId,
+      userId
+  });
+
+    
+  const addUserToOnlineUsersList = ({socketId,userId,activeChat=""}) => {
+    if (!isUserAlreadyOnline(socketId)) {
+      onlineUsers[socketId] = {userId,activeChat}
+   
+    }
+  };
+
   module.exports = SocketManager
