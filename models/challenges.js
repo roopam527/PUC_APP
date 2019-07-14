25e8acd850eb5cfbccba8320f089f45c6e170068
Himanshu507 <himanshuroy507@outlook.com>
2019-07-14 12:45:10 +0530

    Something change

--- models/challenges.js
+++ models/challenges.js
@@ -3,25 +3,28 @@ const Schema = mongoose.Schema;
 const uniqueValidator = require('mongoose-unique-validator');
 const status = require('../utility/challengStatus');
 const challengeSchema = mongoose.Schema({
-	title :  {type:String , required:true},
-    description : {type: String,required:true},
-    creator:{
-        required:true,
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    creator: {
+        required: true,
         type: Schema.Types.ObjectId,
         ref: 'users'
     },
-    filetype : {type:String,required:true},
-    given_to:[{
-        user:{  
-            type: [Schema.Types.ObjectId],
-            ref: 'users'
-        },
-        status:{
-            type:String,
-            default:status[0]
-        } ,
-        default:[]
-    }],
+    filetype: { type: String, required: true },
+    given_to: {
+        type: [{
+            user: {
+                type: [Schema.Types.ObjectId],
+                ref: 'users'
+            },
+            status: {
+                type: String,
+                default: status[0]
+            },
+          
+        }],
+        default: []
+    }
 });
 challengeSchema.plugin(uniqueValidator);
-module.exports = mongoose.model("challenges",challengeSchema);
+module.exports = mongoose.model("challenges", challengeSchema);
