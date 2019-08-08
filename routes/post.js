a0baea84ebaa8f323ce04bf41b3128479827bfd6
saksham90 <sakshamsidh@gmail.com>
2019-08-08 16:19:53 +0530

    added try catch

--- routes/post.js
+++ routes/post.js
@@ -6,54 +6,91 @@ const doneChallenges = mongoose.model("doneChallenges");
 const User = mongoose.model("users");
 const requireLogin = require("../middlewares/requireLogin");
 const { isLength } = require('validator');
-
+let emojis = [
+    "cry",
+    "laugh",
+    "sad",
+    "love",
+]
 router.post("/comment", requireLogin, async (req, res, next) => {
   if(!isLength(req.body.comment, { min: 1 })){
     return res.status(200).json({
         message: "Invalid comment"
     });
   }
-
-  const doneChallenge = new doneChallenges({
-      _id: req.body.id
-  })
-  doneChallenge.update({ 
-      $push: { comments: { user_id: req.userData.userId, comment: req.body.comment, createdAt: JSON.parse(JSON.stringify(Date.now())) } } , 
-    }).then(() => {
-        res.status(200).json({
-        message: "Challenge added successfully"
-        });
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({
-      message: "Challenge creation failed"
+  try{
+    const doneChallenge = await doneChallenges.findById(req.body.id);
+    await doneChallenge.update({ 
+        $push: { comments: { user_id: req.userData.userId, comment: req.body.comment } } , 
+      })
+    res.status(200).json({
+    message: "comment added successfully"
     });
-  });
-
+  } catch(err){
+    res.status(200).json({
+        message: "comment creation failed"
+    });
+  }
 });
 
 router.get("/comments", requireLogin, async (req, res, next) => {
     if(!req.query.id){
         return res.status(200).json({
-            message: "Invalid channel id"
+            message: "Invalid post id"
         });
     }
-    const doneChallenge = await doneChallenges.findById(req.query.id);
-    const comments = await Promise.all(doneChallenge.comments.map(async (c) => {
-        let comment = {
-            comment: c.comment,
-            createdAt: c.createdAt
-        }
-        const user = await User.findById(c.user_id);
-        comment.profile_pic = user.profile_pic;
-        comment.username = user.username;
-        return comment;
-    }));
-    res.status(200).json({
-        comments: await comments
-    });
+    try{
+        const doneChallenge = await doneChallenges.findById(req.query.id);
+        const comments = await Promise.all(doneChallenge.comments.map(async (c) => {
+            let comment = {
+                comment: c.comment,
+                createdAt: c.createdAt
+            }
+            const user = await User.findById(c.user_id);
+            comment.profile_pic = user.profile_pic;
+            comment.username = user.username;
+            return comment;
+        }));
+        res.status(200).json({
+            comments: await comments
+        });
+    } catch(err) {
+        return res.status(200).json({
+            message: "Unable to fetch the comments"
+        });
+    }
 });
+
+//Likes
+router.post("/like", requireLogin, async (req, res, next) => {
+    if(!req.body.id){
+      return res.status(200).json({
+          message: "Invalid post id"
+      });
+    }
+
+    if(emojis.indexOf(req.body.emoji) == -1){
+        return res.status(200).json({
+            message: "Invalid emoji"
+        });
+    }
+  
+    let doneChallenge = await doneChallenges.findById(req.body.id);
+    doneChallenge.update({ 
+        $push: { likes: { user_id: req.userData.userId, emoji: req.body.emoji } } , 
+      }, { upsert: true }).then(() => {
+          res.status(200).json({
+          message: "Like added successfully"
+          });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Like creation failed"
+      });
+    });
+  
+  });
 
 
 module.exports = router;
